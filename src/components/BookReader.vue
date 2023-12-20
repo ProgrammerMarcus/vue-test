e53cca18c096fe67a10e2694e265a9c2a09dfd7e
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2023-12-20 15:10:05 +0100

    removed some scaff

---
+++ src/components/BookReader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  title: string, text: string
+}>()
+</script>
+
+<template>
+    <main>
+        <h2>
+            {{ title }}
+        </h2>
+        <p>
+            {{ text }}
+        </p>
+    </main>
+</template>
+
+<style scoped>
+    main {
+        width: 512px;
+        height: 512px;
+        background-color: aqua;
+    }
+</style>
